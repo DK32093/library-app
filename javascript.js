d67cbea8949685fe36e7565ce55c5a2b4f9689f7
DK32093 <107143112+DK32093@users.noreply.github.com>
2024-09-25 12:46:46 -0400

    create displayLibrary function

--- javascript.js
+++ javascript.js
@@ -1,4 +1,4 @@
-const myLibrary = [];
+const myLibrary = [{title: "The Hobbit", author: "J.R.R. Tolkien", pages: "800", read: "Yes"}];
 const dialog = document.querySelector("dialog");
 const bookButton = document.querySelector(".bookButton");
 const submitButton = document.querySelector(".submitButton");
@@ -15,18 +15,30 @@ function addBookToLibrary() {
     const newAuthor = document.querySelector("#author").value;
     const newPage = document.querySelector("#pageCount").value;
     const newRead = document.querySelector('input[name="read"]:checked').value;
-    const newBook = new Book(newTitle, newAuthor, newPage, newRead)
+    const newBook = new Book(newTitle, newAuthor, newPage, newRead);
     myLibrary.push(newBook);
 }
 
-// "Show the new book form
+function displayLibrary() {
+    const libDisplay = document.querySelector('.library-display');
+    libDisplay.textContent = ""; // reset display first
+    for (const book of myLibrary) {
+        let card = document.createElement('div');
+        card.textContent = book.title
+        libDisplay.appendChild(card);
+    }
+}
+
+// Show the new book form
 bookButton.addEventListener("click", () => {
   dialog.showModal();
 });
 
-// Close the form after submitting
+// Close the form and add new book after submitting
 submitButton.addEventListener("click", () => {
   dialog.close();
   addBookToLibrary();
-  console.log(myLibrary)
+  displayLibrary();
 });
+
+displayLibrary();
