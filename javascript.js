fc9fa38ffc148f4fcdcc4c4d8e1d07eab39e9999
DK32093 <107143112+DK32093@users.noreply.github.com>
2024-09-25 17:31:03 -0400

    add removeBook function and listener

--- javascript.js
+++ javascript.js
@@ -1,7 +1,12 @@
-const myLibrary = [{title: "The Hobbit", author: "J.R.R. Tolkien", pages: "800", read: "Yes"}];
+const myLibrary = [{title: "The Hobbit", author: "J.R.R. Tolkien", pages: "800", read: "Yes"},
+    {title: "The Hobbit", author: "J.R.R. Tolkien", pages: "800", read: "Yes"},
+    {title: "The Hobbit", author: "J.R.R. Tolkien", pages: "800", read: "Yes"}
+];
 const dialog = document.querySelector("dialog");
 const bookButton = document.querySelector(".bookButton");
 const submitButton = document.querySelector(".submitButton");
+const removeButton = document.getElementsByClassName(".removeButton");
+const libDisplay = document.querySelector('.library-display');
 
 function Book(title, author, pages, read) {
   this.title = title;
@@ -20,14 +25,38 @@ function addBookToLibrary() {
 }
 
 function displayLibrary() {
-    const libDisplay = document.querySelector('.library-display');
     libDisplay.textContent = ""; // reset display first
-    for (const book of myLibrary) {
+    for (const [i, book] of myLibrary.entries()) { //entries retrieves index (i)
+        book.index = i; // add index as a property for each object
         let card = document.createElement('div');
-        card.textContent = book.title
+        card.setAttribute("class", "book-card");
         libDisplay.appendChild(card);
+        let title = document.createElement("h2");
+        let author = document.createElement("h3");
+        let pages = document.createElement("p");
+        let read = document.createElement("p");
+        let remove = document.createElement("button")
+        remove.setAttribute("class", "removeButton");
+        remove.setAttribute("data", i); // add index as attribute
+        title.textContent = book.title;
+        author.textContent = "by " + book.author;
+        pages.textContent = "Page count: " + book.pages;
+        read.textContent = "Read: " + book.read;
+        remove.textContent = "Remove";
+        card.append(title, author, pages, read, remove);
+        console.log(i);
     }
 }
+
+function removeBook(e) {
+    const ind = Number(e.target.getAttribute("data"));
+    myLibrary.splice(ind, 1);
+    displayLibrary();
+}
+
+libDisplay.addEventListener("click", (e) => {
+    removeBook(e);
+})
 
 // Show the new book form
 bookButton.addEventListener("click", () => {
@@ -40,5 +69,6 @@ submitButton.addEventListener("click", () => {
   addBookToLibrary();
   displayLibrary();
 });
+
 
 displayLibrary();
